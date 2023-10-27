9aed9245dd8ac016434a58dfba34168231c56d33
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2023-10-27 15:53:43 -0700

    Add order model

--- petshop/store/models.py
+++ petshop/store/models.py
@@ -9,6 +9,24 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.name
 
-# class Order(models.Model):
-#     # only order 1 thing for now
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = "PE"
+        APPROVED = "AP"
+        FULFILLED = "FU"
+    
+    status = models.CharField(
+        max_length=2,
+        choices=OrderStatus.choices,
+        default=OrderStatus.PENDING
+    )
+    complete = models.BooleanField(default=False)
+    order_lines = models.ManyToManyField(Product, through="Orderline")
+
+
+class OrderLine(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     
