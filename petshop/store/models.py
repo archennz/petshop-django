2c9ddfc46c5262cd565a35c7cfb477bade89eae5
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2023-10-26 18:30:43 -0700

    Add product model to db

--- petshop/store/models.py
+++ petshop/store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=400)
+    quantity = models.IntegerField()
+
+# class Order(models.Model):
+#     # only order 1 thing for now
+    
