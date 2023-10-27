0806023894456ae9418fe6aee1b0eb0184e5cab9
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2023-10-26 18:36:20 -0700

    Add product to admin view

--- petshop/store/models.py
+++ petshop/store/models.py
@@ -6,6 +6,9 @@ class Product(models.Model):
     name = models.CharField(max_length=400)
     quantity = models.IntegerField()
 
+    def __str__(self) -> str:
+        return self.name
+
 # class Order(models.Model):
 #     # only order 1 thing for now
     
