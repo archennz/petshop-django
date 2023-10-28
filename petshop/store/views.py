9e1eb5a9471e860db69908bb4eadd8a052063d86
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2023-10-27 17:12:53 -0700

    Add barebones store view

--- petshop/store/views.py
+++ petshop/store/views.py
@@ -1,6 +1,16 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Product
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the index")
+class IndexView(generic.ListView):
+    template_name = "store/index.html"
+    context_object_name = "products"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Product.objects.get_queryset()
+
