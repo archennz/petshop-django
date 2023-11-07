e252421f69a9688da7b4169ee40838e823d8f5ae
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2023-11-06 17:11:49 -0800

    Add some E2E tests

--- petshop/store/tests/test_models.py
+++ petshop/store/tests/test_models.py
@@ -1,5 +1,9 @@
 import unittest
 from django.utils import timezone
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase
 from ..models import Order
 
 class TestOrder(unittest.TestCase):
@@ -24,3 +28,26 @@ class TestOrder(unittest.TestCase):
             date = date,
             shipping_number = shipping_number
         )
+
+
+class Test_E2E(APITestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        Order.objects.create()
+        return super().setUpClass()
+
+    def test_get_orders(self):
+        response = self.client.get('/api/orders/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    
+    def test_update_shipping_with_bad_data_returns_bad_request(self):
+        response = self.client.post('/api/orders/1/complete_order/', data={})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    
+    def test_update_shipping_with_bad_data_returns_bad_request(self):
+        response = self.client.post('/api/orders/1/complete_order/', data={'shipping_number': 1})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
